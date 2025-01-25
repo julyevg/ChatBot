5c014b392fe5e49293c5dc5da0b4df9146da9a7b
July Suarez <julysuarez@MacBook-Pro-de-July.local>
2025-01-25 10:44:09 -0500

    first commit

---
+++ hola.py
@@ -0,0 +1,25 @@
+##### LISTAS #####
+frutas = ["manzana", "platano", "cerezo"]
+
+frutas.append("naranja")
+frutas[2] = "cereza"
+
+# print(frutas)
+
+##### TUPLAS #####
+# valores fijos
+dias_semana = ("lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo")
+
+##### DICCIONARIOS #####
+persona = {
+    "edad": 23,
+    "nombre": "Juan",
+    "fecha_de_nacimiento": "2002-06-19",
+    "ciudad": "Lima"
+}
+
+persona["edad"] = 32
+persona["profesion"] = "ingeniero"
+
+# print(persona.get("nombre"))
+# print(persona["profesion"])
